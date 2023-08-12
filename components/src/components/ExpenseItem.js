98185f0ee60511e5b93dfdf86c416ef129a15f88
basichacker881 <mugdhasharma0327@gmail.com>
2023-08-12 16:53:11 +0530

    props + styling

--- components/src/components/ExpenseItem.js
+++ components/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
-function ExpenseItem() {
+import './ExpenseItem.css'
+
+//props is an object that contains all the attributes of the component and makes it reusable
+
+function ExpenseItem(props) {
   //only one root element is allowed per component
   return (
-    <div>
-      <div>Date</div>
-      <div>
-        <h2>Title</h2>
+    <div className="expense-item">
+      <div className="expense-item__date">{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
       </div>
-      <div>Amount</div>
+      <div className="expense-item__price">${props.price}</div>
     </div>
   );
 }
