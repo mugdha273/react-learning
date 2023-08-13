0f33d51fa0c237c27c52556ca9b8665f5481316a
basichacker881 <mugdhasharma0327@gmail.com>
2023-08-13 23:21:42 +0530

    props complete

--- components/src/App.js
+++ components/src/App.js
@@ -1,4 +1,6 @@
 import ExpenseItem from './components/ExpenseItem';
+import './components/Expenses.css';
+import Card from './components/Card';
 
 function App() {
   const expenses = [
@@ -9,11 +11,13 @@ function App() {
   ];
   return (
     <div>
-      <h2>Expense Tracker</h2>
+    <h2>Expense Tracker</h2>
+    <Card className="expenses">
       <ExpenseItem title={expenses[0].title} price = {expenses[0].amount} date = {expenses[0].date}></ExpenseItem>
       <ExpenseItem title={expenses[1].title} price = {expenses[1].amount} date = {expenses[1].date}></ExpenseItem>
       <ExpenseItem title={expenses[2].title} price = {expenses[2].amount} date = {expenses[2].date}></ExpenseItem>
       <ExpenseItem title={expenses[3].title} price = {expenses[3].amount} date = {expenses[3].date}></ExpenseItem>
+    </Card>
     </div>
   );
 }
--- components/src/components/ExpenseItem.js
+++ components/src/components/ExpenseItem.js
@@ -1,17 +1,23 @@
-import './ExpenseItem.css'
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate.js';
+import Card from './Card';
 
 //props is an object that contains all the attributes of the component and makes it reusable
+//props bundles all the attributes of the component into one object
 
 function ExpenseItem(props) {
   //only one root element is allowed per component
   return (
-    <div className="expense-item">
-      <div className="expense-item__date">{props.date.toISOString()}</div>
+    <Card className="expense-item">
+      <div className="expense-item__date">
+        {/* if you have no content in the div, you can use self closing tag */}
+        <ExpenseDate date = {props.date}/>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
       <div className="expense-item__price">${props.price}</div>
-    </div>
+    </Card>
   );
 }
 
